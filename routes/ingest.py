72f3fb591380b38c79402b49b80d8aaf5191225b
imshubhamkj <imshubhamkj@gmail.com>
2024-12-08 23:26:01 +0530

    changes made to transefr file to a local location on the server and transfer file meta data to postgressql

--- routes/ingest.py
+++ routes/ingest.py
@@ -1,24 +1,40 @@
-from flask import Blueprint,jsonify,request
+from flask import Blueprint, abort,jsonify,request
 from models.doument import Document
 from models import db
+from werkzeug.utils import secure_filename
+from config import Constants
+import os
 
 ingest_blueprint = Blueprint('ingest',__name__)
+UPLOAD_FOLDER = Constants.UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok = True)
+
+
 
 @ingest_blueprint.route('/test',methods = ['GET'])
 def test_db_connection():
     documents = Document.query.all()
-    return jsonify([{"id":doc.id,"title":doc.title,"content":doc.content} for doc in documents])
+    return jsonify([{"id":doc.id,"title":doc.title,"content":doc.file_path} for doc in documents])
 
 @ingest_blueprint.route('/', methods = ['POST'])
 def ingest_document():
     try:
-        data = request.get_json()
-        title = data.get('title')
-        content = data.get('content')
+        if 'file' not in request.files:
+            abort(400)
+        file = request.files['file']
+        # data = request.get_json()
+        title = request.form.get('title')
+        # content = data.get('content')
+        print(file,title)
 
-        if not title or not content:
-            return jsonify({"error":"both title and content are required"}),400
-        new_document = Document(title=title,content=content)
+        if not title or file.filename =='':
+            return jsonify({"error":"both title and file are required"}),400
+        
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(UPLOAD_FOLDER,filename)
+        file.save(file_path)
+
+        new_document = Document(title=title,file_path=file_path)
         db.session.add(new_document)
         db.session.commit()
 
